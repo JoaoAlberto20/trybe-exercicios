d1e2e56708800160c6d1ec60d004f3436f3a66cf
Joao Alberto <joaoalbertosv15@gmail.com>
2022-02-09 18:59:52 -0300

    Adicionando arquivos

---
+++ fundamentos/bloco-04-introducao-a-java-script-logica-de-programacao/dia-4-java-scripts-objetos-e-funcao/exercicios-2-de-ficcao.js
@@ -0,0 +1,15 @@
+let player = {
+
+    names: 'Marta',
+    lastName:'Silva',
+    age : 34,
+    medals: { 
+        
+      golden: 2,
+      sillver: 3,
+
+    }
+
+}
+    
+console.log('A jogadora ' + player['names'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' de idade');
---
+++ fundamentos/bloco-04-introducao-a-java-script-logica-de-programacao/dia-4-java-scripts-objetos-e-funcao/exercicios-3-de-ficcao.js
@@ -0,0 +1,21 @@
+/* 
+let player = {
+
+    names: 'Marta',
+    lastName:'Silva',
+    age : 34,
+    medals: { 
+        
+      golden: 2,
+      sillver: 3,
+
+    }
+
+    bestInTheWorld :  [2006, 2007, 2008, 2009, 2010, 2018],
+
+};
+    
+/* console.log('A jogadora ' + player['names'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' de idade'); */
+
+
+
