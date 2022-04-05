d2c395730ed3ef70d8324d5a2f243f5fa9f78171
Joao Alberto <joaoalbertosv15@gmail.com>
2022-04-05 14:47:47 -0300

    Adicionando primeiro exercicios de Front End

---
+++ FRONT-END/bloco-10-introducao-a-react/dia-1-introducao/my-2-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Content from './content';
+import Footer  from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
